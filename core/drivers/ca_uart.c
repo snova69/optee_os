003798f17f9a73b8937c2f5f1a0650f6b8c59df9
Alex Nemirovsky <alex.nemirovsky@cortina-access.com>
2024-09-25 23:15:40 -0700

    ca_uart: initial commit

--- core/drivers/ca_uart.c
+++ core/drivers/ca_uart.c
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2014, Linaro Limited
+ * Copyright (c) 2024, Cortina Access Inc.
  */
 #include <assert.h>
-#include <drivers/pl011.h>
+#include <drivers/ca_uart.h>
 #include <io.h>
 #include <util.h>
 #include <keep.h>
@@ -15,77 +15,16 @@
 #include <types_ext.h>
 #include <util.h>
 
-#define UART_DR		0x00 /* data register */
-#define UART_RSR_ECR	0x04 /* receive status or error clear */
-#define UART_DMAWM	0x08 /* DMA watermark configure */
-#define UART_TIMEOUT	0x0C /* Timeout period */
-/* reserved space */
-#define UART_FR		0x18 /* flag register */
-#define UART_ILPR	0x20 /* IrDA low-poer */
-#define UART_IBRD	0x24 /* integer baud register */
-#define UART_FBRD	0x28 /* fractional baud register */
-#define UART_LCR_H	0x2C /* line control register */
-#define UART_CR		0x30 /* control register */
-#define UART_IFLS	0x34 /* interrupt FIFO level select */
-#define UART_IMSC	0x38 /* interrupt mask set/clear */
-#define UART_RIS	0x3C /* raw interrupt register */
-#define UART_MIS	0x40 /* masked interrupt register */
-#define UART_ICR	0x44 /* interrupt clear register */
-#define UART_DMACR	0x48 /* DMA control register */
-
-/* flag register bits */
-#define UART_FR_RTXDIS	(1 << 13)
-#define UART_FR_TERI	(1 << 12)
-#define UART_FR_DDCD	(1 << 11)
-#define UART_FR_DDSR	(1 << 10)
-#define UART_FR_DCTS	(1 << 9)
-#define UART_FR_RI	(1 << 8)
-#define UART_FR_TXFE	(1 << 7)
-#define UART_FR_RXFF	(1 << 6)
-#define UART_FR_TXFF	(1 << 5)
-#define UART_FR_RXFE	(1 << 4)
-#define UART_FR_BUSY	(1 << 3)
-#define UART_FR_DCD	(1 << 2)
-#define UART_FR_DSR	(1 << 1)
-#define UART_FR_CTS	(1 << 0)
-
-/* transmit/receive line register bits */
-#define UART_LCRH_SPS		(1 << 7)
-#define UART_LCRH_WLEN_8	(3 << 5)
-#define UART_LCRH_WLEN_7	(2 << 5)
-#define UART_LCRH_WLEN_6	(1 << 5)
-#define UART_LCRH_WLEN_5	(0 << 5)
-#define UART_LCRH_FEN		(1 << 4)
-#define UART_LCRH_STP2		(1 << 3)
-#define UART_LCRH_EPS		(1 << 2)
-#define UART_LCRH_PEN		(1 << 1)
-#define UART_LCRH_BRK		(1 << 0)
-
-/* control register bits */
-#define UART_CR_CTSEN		(1 << 15)
-#define UART_CR_RTSEN		(1 << 14)
-#define UART_CR_OUT2		(1 << 13)
-#define UART_CR_OUT1		(1 << 12)
-#define UART_CR_RTS		(1 << 11)
-#define UART_CR_DTR		(1 << 10)
-#define UART_CR_RXE		(1 << 9)
-#define UART_CR_TXE		(1 << 8)
-#define UART_CR_LPE		(1 << 7)
-#define UART_CR_OVSFACT		(1 << 3)
-#define UART_CR_UARTEN		(1 << 0)
-
-#define UART_IMSC_RTIM		(1 << 6)
-#define UART_IMSC_RXIM		(1 << 4)
 
 static vaddr_t chip_to_base(struct serial_chip *chip)
 {
-	struct pl011_data *pd =
-		container_of(chip, struct pl011_data, chip);
+	struct ca_uart_data *pd =
+		container_of(chip, struct ca_uart_data, chip);
 
-	return io_pa_or_va(&pd->base, PL011_REG_SIZE);
+	return io_pa_or_va(&pd->base, CA_UART_REG_SIZE);
 }
 
-static void pl011_flush(struct serial_chip *chip)
+static void ca_uart_flush(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
@@ -96,156 +35,222 @@ static void pl011_flush(struct serial_chip *chip)
 	 * enabled flag. Checking it in the loop makes the code safe against
 	 * asynchronous disable.
 	 */
-	while ((io_read32(base + UART_CR) & UART_CR_UARTEN) &&
-	       !(io_read32(base + UART_FR) & UART_FR_TXFE))
+
+	while ((io_read32(base + CFG) & (CFG_UART_EN | CFG_TX_EN | CFG_RX_EN)) &&
+ 		( !(io_read32(base + INFO) & INFO_TX_EMPTY)))
 		;
 }
 
-static bool pl011_have_rx_data(struct serial_chip *chip)
+static bool ca_uart_have_rx_data(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	return !(io_read32(base + UART_FR) & UART_FR_RXFE);
+	return !(io_read32(base + INFO) & INFO_RX_EMPTY);
 }
 
-static int pl011_getchar(struct serial_chip *chip)
+static int ca_uart_getchar(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (!pl011_have_rx_data(chip))
+	while (!ca_uart_have_rx_data(chip))
 		;
-	return io_read32(base + UART_DR) & 0xff;
+	return(io_read32(base + UART_DR) & 0xff);
 }
 
-static void pl011_putc(struct serial_chip *chip, int ch)
+static void ca_uart_putc(struct serial_chip *chip, int ch)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	/* Wait until there is space in the FIFO or device is disabled */
-	while (io_read32(base + UART_FR) & UART_FR_TXFF)
+	/* wait for transmitter to empty to accept a character */
+	while (!io_read32(base + INFO) & INFO_TX_EMPTY)
 		;
-
 	/* Send the character */
-	io_write32(base + UART_DR, ch);
+	io_write32(base + TX_DAT, ch);
 }
 
-static void pl011_rx_intr_enable(struct serial_chip *chip)
+static void ca_uart_rx_intr_enable(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
+	uint32_t temp;
 
-	io_write32(base + UART_IMSC, UART_IMSC_RXIM);
+        temp = io_read32(base + IE);
+        temp |= UINT_RX_ALL;
+        io_write32(port->membase + IE, temp);
 }
 
-static void pl011_rx_intr_disable(struct serial_chip *chip)
+static void ca_uart_rx_intr_disable(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
+	uint32_t temp;
 
-	io_write32(base + UART_IMSC, 0);
+        temp = io_read32(base + IE);
+	temp &= ~(UINT_RX_ALL);
+        io_write32(port->membase + IE, temp);
 }
 
-static const struct serial_ops pl011_ops = {
-	.flush = pl011_flush,
-	.getchar = pl011_getchar,
-	.have_rx_data = pl011_have_rx_data,
-	.putc = pl011_putc,
-	.rx_intr_enable = pl011_rx_intr_enable,
-	.rx_intr_disable = pl011_rx_intr_disable,
+static const struct serial_ops ca_uart_ops = {
+	.flush = ca_uart_flush,
+	.getchar = ca_uart_getchar,
+	.have_rx_data = ca_uart_have_rx_data,
+	.putc = ca_uart_putc,
+	.rx_intr_enable = ca_uart_rx_intr_enable,
+	.rx_intr_disable = ca_uart_rx_intr_disable,
 };
-DECLARE_KEEP_PAGER(pl011_ops);
+DECLARE_KEEP_PAGER(ca_uart_ops);
 
-void pl011_init(struct pl011_data *pd, paddr_t pbase, uint32_t uart_clk,
+void ca_uart_init(struct ca_uart_data *pd, paddr_t pbase, uint32_t uart_clk,
 		uint32_t baud_rate)
 {
 	vaddr_t base;
 
 	pd->base.pa = pbase;
-	pd->chip.ops = &pl011_ops;
+	pd->chip.ops = &ca_uart_ops;
 
-	base = io_pa_or_va(&pd->base, PL011_REG_SIZE);
+	base = io_pa_or_va(&pd->base, CA_UART_REG_SIZE);
 
-	/* Clear all errors */
-	io_write32(base + UART_RSR_ECR, 0);
-	/* Disable everything */
-	io_write32(base + UART_CR, 0);
+        uint32_t temp; /* temp cfg */
+        uint32_t sample_fre = 0;
 
-	if (baud_rate) {
-		uint32_t divisor = (uart_clk * 4) / baud_rate;
+	/* Turn off Tx interrupts. The port lock is held at this point */
+        temp = io_read32(base + IE);
+        temp &= ~(UINT_TX_ALL);
+        io_write32(membase + IE, temp);
 
-		io_write32(base + UART_IBRD, divisor >> 6);
-		io_write32(base + UART_FBRD, divisor & 0x3f);
-	}
+        /* Turn off Rx interrupts. The port lock is held at this point */
+        temp = io_read32(base + IE);
+        temp &= ~(UINT_RX_ALL);
+        io_write32(base + IE, temp);
 
-	/* Configure TX to 8 bits, 1 stop bit, no parity, fifo disabled. */
-	io_write32(base + UART_LCR_H, UART_LCRH_WLEN_8);
 
-	/* Enable receive interrupt */
-	io_write32(base + UART_IMSC, UART_IMSC_RXIM);
+        if ((baud_rate != 0) && (uart_clk != 0)) {
+
+        	temp = io_read32(base + CFG);
+                /* mask off the baud settings held in the top 24 bits */
+                temp &= 0xff;
+                dev_data->baud_rate = baud_rate;
+
+                switch (baud_rate) {
+                 case 9600:
+                        temp |= (uart_clk / 9600) << CFG_BAUD_SART ;
+                        break;
+                 case 19200:
+                        temp |= (uart_clk / 19200) << CFG_BAUD_SART ;
+                        break;
+                 case 38400:
+                        temp |= (uart_clk / 38400) << CFG_BAUD_SART ;
+                        break;
+                 case 57600:
+                        temp |= (uart_clk / 57600) << CFG_BAUD_SART ;
+                        break;
+                 case 115200:
+                        temp |= (uart_clk / 115200) << CFG_BAUD_SART ;
+                        break;
+                 default:
+                        temp |= (uart_clk / 38400) << CFG_BAUD_SART ;
+                        break;
+                }
+
+                /* Sampling rate should be half of baud count */
+                sample_fre = (temp >> CFG_BAUD_SART) / 2;
+                io_write32(base + CFG, temp);
+                io_write32(base + RX_SAMPLE, sample_fre);
+        }
+
+	/* set 8,N,1 */
+        temp = io_read32(base + CFG);
+
+	/* 8 data bits */
+        /* mask off the data width */
+        temp &= 0xfffffffc;
+        temp |= 0x3;
+
+        /* 1 stop bit */
+        /* mask off the 2 Stop enable bit */
+        temp &= ~(CFG_STOP_2BIT);
+
+        /* no parity */
+        /* mask off the parity enable bit */
+        temp &= ~(CFG_PARITY_EN);
+        io_write32(base + CFG, temp);
 
 	/* Enable UART and RX/TX */
-	io_write32(base + UART_CR, UART_CR_UARTEN | UART_CR_TXE | UART_CR_RXE);
+        temp = io_read32(base + CFG);
+        io_write32(base + CFG, (temp | CFG_UART_EN | CFG_TX_EN | CFG_RX_EN));
 
-	pl011_flush(&pd->chip);
+	ca_uart_flush(&pd->chip);
 }
 
 #ifdef CFG_DT
 
-static struct serial_chip *pl011_dev_alloc(void)
+static struct serial_chip *ca_uart_dev_alloc(void)
 {
-	struct pl011_data *pd = nex_calloc(1, sizeof(*pd));
+	struct ca_uart_data *pd = nex_calloc(1, sizeof(*pd));
 
 	if (!pd)
 		return NULL;
 	return &pd->chip;
 }
 
-static int pl011_dev_init(struct serial_chip *chip, const void *fdt, int offs,
+static int ca_uart_dev_init(struct serial_chip *chip, const void *fdt, int offs,
 			  const char *parms)
 {
-	struct pl011_data *pd = container_of(chip, struct pl011_data, chip);
+	struct ca_uart_data *pd = container_of(chip, struct ca_uart_data, chip);
 	vaddr_t vbase;
 	paddr_t pbase;
 	size_t size;
+	uint32_t uart_clock;
+	const fdt32_t *cuint = NULL;
 
 	if (parms && parms[0])
-		IMSG("pl011: device parameters ignored (%s)", parms);
+		IMSG("ca_uart: device parameters ignored (%s)", parms);
 
 	if (dt_map_dev(fdt, offs, &vbase, &size, DT_MAP_AUTO) < 0)
 		return -1;
 
 	if (size != 0x1000) {
-		EMSG("pl011: unexpected register size: %zx", size);
+		EMSG("ca_uart: unexpected register size: %zx", size);
 		return -1;
 	}
 
+	cuint = fdt_getprop(fdt, node, "clocks", NULL);
+        if (cuint) {
+                uart_clock = fdt32_to_cpu(*cuint);
+		IMSG("ca_uart: uart_clock is (%zx)", uart_clock);
+
+        } else {
+		EMSG("ca_uart: uart_clock not found in DTS");
+		return -1;
+        }
+
 	pbase = virt_to_phys((void *)vbase);
-	pl011_init(pd, pbase, 0, 0);
+	ca_uart_init(pd, pbase, 115200, uart_clock);
 
 	return 0;
 }
 
-static void pl011_dev_free(struct serial_chip *chip)
+static void ca_uart_dev_free(struct serial_chip *chip)
 {
-	struct pl011_data *pd = container_of(chip, struct pl011_data, chip);
+	struct ca_uart_data *pd = container_of(chip, struct ca_uart_data, chip);
 
 	nex_free(pd);
 }
 
-static const struct serial_driver pl011_driver = {
-	.dev_alloc = pl011_dev_alloc,
-	.dev_init = pl011_dev_init,
-	.dev_free = pl011_dev_free,
+static const struct serial_driver ca_uart_driver = {
+	.dev_alloc = ca_uart_dev_alloc,
+	.dev_init = ca_uart_dev_init,
+	.dev_free = ca_uart_dev_free,
 };
 
-static const struct dt_device_match pl011_match_table[] = {
-	{ .compatible = "arm,pl011" },
+static const struct dt_device_match ca_uart_match_table[] = {
+	{ .compatible = "cortina-access,serial" },
 	{ 0 }
 };
 
-DEFINE_DT_DRIVER(pl011_dt_driver) = {
-	.name = "pl011",
+DEFINE_DT_DRIVER(ca_uart_dt_driver) = {
+	.name = "ca_uart",
 	.type = DT_DRIVER_UART,
-	.match_table = pl011_match_table,
-	.driver = &pl011_driver,
+	.match_table = ca_uart_match_table,
+	.driver = &ca_uart_driver,
 };
 
 #endif /* CFG_DT */
--- core/include/drivers/ca_uart.h
+++ core/include/drivers/ca_uart.h
@@ -1,21 +1,83 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2014, Linaro Limited
+ * Copyright (c) 2024, Cortina Access
  */
-#ifndef __DRIVERS_PL011_H
-#define __DRIVERS_PL011_H
+#ifndef __DRIVERS_CS_UART_H
+#define __DRIVERS_CS_UART_H
 
 #include <types_ext.h>
 #include <drivers/serial.h>
 
-#define PL011_REG_SIZE	0x1000
+#define CS_UART_REG_SIZE	0x0030
 
-struct pl011_data {
+/* Register definitions */
+#define UCFG                    0x00 	/* UART config register */
+#define UFC                     0x04	/* flow control */
+#define URX_SAMPLE              0x08 	/* UART RX Sample register */
+#define URT_TUNE                0x0C 	/* Fine tune of UART clk */
+#define UTX_DATA                0x10	/* UART TX Character data */
+#define URX_DATA                0x14	/* UART RX Character data */
+#define UINFO                   0x18	/* UART Info */
+#define UINT_EN                 0x1C	/* UART Interrupt enable */
+#define UINT_CLR                0x24	/* UART Interrupt setting/clearing */
+#define UINT_STAT               0x2C	/* UART Interrupt Status */
+
+/* UART Control Register Bit Fields.*/
+#define UCFG_BAUD_COUNT                 (8)
+#define UCFG_EN                         (1 << 7)
+#define UCFG_RX_EN                      (1 << 6)
+#define UCFG_TX_EN                      (1 << 5)
+#define UCFG_PARITY_EN                  (1 << 4)
+#define UCFG_PARITY_SEL                 (1 << 3)
+#define UCFG_2STOP_BIT                  (1 << 2)
+#define UCFG_CNT1                       (1 << 1)
+#define UCFG_CNT0                       (1 << 0)
+#define UCFG_CHAR_5                     0
+#define UCFG_CHAR_6                     1
+#define UCFG_CHAR_7                     2
+#define UCFG_CHAR_8                     3
+
+#define  UINFO_TX_FIFO_EMPTY            (1<<3)
+#define  UINFO_TX_FIFO_FULL             (1 << 2)
+#define  UINFO_RX_FIFO_EMPTY            (1<<1)
+#define  UINFO_RX_FIFO_FULL             (1 << 0)
+
+#define UINT_RX_NON_EMPTY               (1 << 6)
+#define UINT_TX_EMPTY                   (1 << 5)
+#define UINT_RX_UNDERRUN                (1 << 4)
+#define UINT_RX_OVERRUN                 (1 << 3)
+#define UINT_RX_PARITY_ERR              (1 << 2)
+#define UINT_RX_STOP_ERR                (1 << 1)
+#define UINT_TX_OVERRUN                 (1 << 0)
+#define UINT_MASK_ALL                   0x7F
+#define UINT_TX_ALL			0x21
+#define UINT_RX_ALL			0x5E
+
+// UART CONF bits
+#define SHF_UCONF_WL       0
+#define MSK_UCONF_WL       (0x3 << SHF_UCONF_WL)
+#define VAL_UCONF_WL_5     (0x0 << SHF_UCONF_WL)
+#define VAL_UCONF_WL_6     (0x1 << SHF_UCONF_WL)
+#define VAL_UCONF_WL_7     (0x2 << SHF_UCONF_WL)
+#define VAL_UCONF_WL_8     (0x3 << SHF_UCONF_WL)
+
+#define SHF_UCONF_SB       2
+#define MSK_UCONF_SB       (0x1 << SHF_UCONF_SB)
+#define VAL_UCONF_SB_1     (0x0 << SHF_UCONF_SB)
+#define VAL_UCONF_SB_2     (0x1 << SHF_UCONF_SB)
+
+#define SHF_UCONF_PM       3
+#define MSK_UCONF_PM       (0x3 << SHF_UCONF_PM)
+#define VAL_UCONF_PM_N     (0x0 << SHF_UCONF_PM)
+#define VAL_UCONF_PM_O     (0x2 << SHF_UCONF_PM)
+#define VAL_UCONF_PM_E     (0x3 << SHF_UCONF_PM)
+
+struct cs_uart_data {
 	struct io_pa_va base;
 	struct serial_chip chip;
 };
 
-void pl011_init(struct pl011_data *pd, paddr_t pbase, uint32_t uart_clk,
+void cs_uart_init(struct cs_uart_data *pd, paddr_t pbase, uint32_t uart_clk,
 		uint32_t baud_rate);
 
-#endif /* __DRIVERS_PL011_H */
+#endif /* __DRIVERS_CS_UART_H */
